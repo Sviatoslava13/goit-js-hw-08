d974539de64d6bcc61ff0a4404435b05368c6a41
Sviatoslava13 <sviatoslavamar@gmail.com>
2022-09-25 21:44:11 +0300

    home-work

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,19 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
-
+ //import templatesGallery from '../templates/gallery.hbs'; 
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css"; 
 console.log(galleryItems);
+
+const elementGallery = document.querySelector('.gallery');
+
+const markup = galleryItems.map(({ preview, original, description }) =>` <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" style="display: block" />
+</a>`).join('');
+elementGallery.insertAdjacentHTML('afterbegin', markup);
+console.log(markup);
+
+new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    email: document.querySelector('input'),
+    message: document.querySelector('textarea')
+}
+refs.form.addEventListener('submit', onFormSubmit),
+refs.form.addEventListener('input', throttle( onTextareaInput, 500))
+
+const formData = {};
+  
+function onTextareaInput(e) {
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData)) ;
+
+formData[e.target.name] = e.target.value;  
+      console.log(formData);
+}
+
+function onFormSubmit(e) {
+    e.preventDefault();
+
+    console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+
+    e.currentTarget.reset();
+    localStorage.removeItem('feedback-form-state');
+
+   
+}
+function dataForm() {
+    const dataParse = JSON.parse(localStorage.getItem('feedback-form-state'));
+    
+    if (dataParse) {
+               refs.email.value = dataParse.email;
+            refs.message.value = dataParse.message;
+    }
+
+}
+dataForm();
+
+
 
